b81651de886095594552b2b32641446bc4d15db3
YU GAO <solonot@gmail.com>
2011-09-27 02:25:53 +0800

    project init

--- SDemoMain/src/com/sdemo/SDemoGame.java
+++ SDemoMain/src/com/sdemo/SDemoGame.java
@@ -1,45 +1,19 @@
 package com.sdemo;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.Mesh;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes.Usage;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.sdemo.screen.GameMainScreen;
 
-public class SDemoGame implements ApplicationListener {
-	private Mesh trangleMesh;
+public class SDemoGame extends Game {
 
+	@Override
 	public void create() {
-		if (trangleMesh == null) {
-            trangleMesh = new Mesh(true, 3, 3, 
-                    new VertexAttribute(Usage.Position, 3, "a_position"));
-                    
-            trangleMesh.setVertices(new float[] { -0.5f, -0.5f, 0,
-                                            0.5f, -0.5f, 0,
-                                            0, 0.5f, 0 });
-                                        
-            trangleMesh.setIndices(new short[] { 0, 1, 2 });
-        }
+		// TODO Auto-generated method stub
+		this.setScreen(new GameMainScreen());
 	}
 
+	@Override
 	public void render() {
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT); // This cryptic line clears
-													// the screen.
-		trangleMesh.render(GL10.GL_TRIANGLES, 0, 3);
-	}
-
-	public void resize(int width, int height) {
-	}
-
-	public void pause() {
-	}
-
-	public void resume() {
-	}
-
-	public void dispose() {
+		// TODO Auto-generated method stub
+		super.render();
 	}
 }
---
+++ SDemoMain/src/com/sdemo/screen/GameMainScreen.java
@@ -0,0 +1,125 @@
+package com.sdemo.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g3d.loaders.ModelLoaderOld;
+import com.badlogic.gdx.graphics.g3d.loaders.wavefront.ObjLoader;
+import com.badlogic.gdx.graphics.g3d.model.still.StillModel;
+
+public class GameMainScreen implements Screen {
+
+	/** the background texture **/
+	private Texture backgroundTexture;
+	/** perspective camera **/
+	private PerspectiveCamera camera;
+
+	private StillModel model;
+
+	private Mesh mesh;
+	private Texture invaderTexture;
+
+	public GameMainScreen() {
+		super();
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
+
+		FileHandle file = Gdx.files.internal("objs/obj.obj");
+		ObjLoader loader = new ObjLoader();
+		model = loader.loadObj(file, true);
+
+		mesh = ModelLoaderOld.loadObj(Gdx.files.internal("objs/invader.obj")
+				.read());
+		invaderTexture = new Texture(Gdx.files.internal("images/invader.png"),
+				Format.RGB565, true);
+		invaderTexture.setFilter(TextureFilter.MipMap, TextureFilter.Linear);
+	}
+
+	static int y;
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		GL10 gl = Gdx.gl10;
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		gl.glDisable(GL10.GL_DITHER);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glEnable(GL10.GL_CULL_FACE);
+
+		setLighting(gl);
+
+		camera.position.set(0, 0, 0); // camera, sometimes you can treat is as
+										// your eyes, here set the eyes
+										// position.
+		camera.direction.set(0, 0, -1); // the sight direction
+		camera.update();
+		camera.apply(Gdx.gl10);
+
+//		gl.glPushMatrix();
+//		gl.glTranslatef(0, -10, -10);// location of the model which be rendered
+//										// next
+//		model.render();
+//		gl.glPopMatrix();
+
+		invaderTexture.bind();
+		gl.glPushMatrix();
+		gl.glTranslatef(0, -10, -10);
+		mesh.render(GL10.GL_TRIANGLES);
+		gl.glPopMatrix();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		backgroundTexture.dispose();
+		model.dispose();
+	}
+
+	float[] direction = { 1, 0.5f, 0, 0 };
+
+	private void setLighting(GL10 gl) {
+		gl.glEnable(GL10.GL_LIGHTING);
+		gl.glEnable(GL10.GL_LIGHT0);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, direction, 0);
+		gl.glEnable(GL10.GL_COLOR_MATERIAL);
+	}
+
+}
